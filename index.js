e1c43ea40f337fd77afcc66fac71b0b292e7509d
SantoSarker101 <santosarker404@gmail.com>
2024-03-18 15:25:32 +0600

    Created server and created users api

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xrsgd45.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+    const userCollection = client.db('starTechDb').collection('user');
+
+
+    // user related APIs
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email }
+
+      const existingUser = await userCollection.findOne(query)
+      console.log('Existing User', existingUser);
+
+      if (existingUser) {
+        return res.send({ Message: 'User Already Exists' })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+    
+
+    app.patch('/users/admin/:id', async(req,res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Star Tech is Ready for Sale')
+})
+
+app.listen(port, () => {
+  console.log(`Star Tech is Sitting on port ${port}`);
+})
